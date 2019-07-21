c2c2c6d64b1e10f9117a24e89de3ee12d6f8177d
Afroze Khan <a.froze.ak@gmail.com>
2019-07-21 15:35:26 -0400

    displaying google auth sigin status

--- src/components/auth/googleAuth.component.js
+++ src/components/auth/googleAuth.component.js
@@ -3,20 +3,39 @@ import * as log from "loglevel";
 
 class GoogleAuth extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "Google OAuth2 Component",
+            loggedIn: false
+        }
+    }
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             log.info("Google API OAuth2 CB");
+
+            // returns a promise
             window.gapi.client.init({
                 clientId: "28500267218-1bq13u7pl94iui84bhg11u97p9fuapbs.apps.googleusercontent.com",
                 scope: "email"
-
+            }).then(()=> {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                log.debug("this.auth: ", this.auth);
+                this.onAuthChange();
+                this.auth.isSignedIn.listen(this.onAuthChange);
             })
         });
 
     } // end componentDidMount
 
+
+    onAuthChange = () => {
+        this.setState({loggedIn: this.auth.isSignedIn.get()})
+    }
+
     render() {
-        return <div>Google Auth</div>;
+        return <div>Title: {this.state.title} : {this.state.loggedIn.toString()} </div>;
     } // end render
 
 } // end class GoogleAuth
